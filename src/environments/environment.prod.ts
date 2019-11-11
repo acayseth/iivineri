ed50a46596b0e6f99e33fcb33dd648092819d255
Serghei <gorbulea.s@gmail.com>
2019-11-11 21:28:12 +0200

    env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  analytics: {
+    code: 'UA-151517869-1'
+  },
+  giphy: {
+    apiKey: 'Z3p94SeDbOUPrywoq3NdL2CT4V6fEE16',
+    tags: {
+      is: 'friday',
+      not: 'fuck'
+    }
+  }
+};
