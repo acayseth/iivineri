3536c5339766ce93553f16edb03bb301010f6d84
Serghei <gorbulea.s@gmail.com>
2019-11-22 12:48:35 +0200

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -101,7 +101,7 @@ export class HomeComponent implements OnInit {
   }
 
   public onGifError(img: string = '404.gif'): void {
-    // this.gif.url = './assets/images/' + img;
+    this.gif.url = './assets/images/' + img;
   }
 
   public setContentFriday(): void {
