5ca3502c300bc4bc19babfc93b04069f8e75247b
acayseth <gorbulea.s@gmail.com>
2023-08-14 15:23:26 +0300

    hotfix in webkit autoplay player and get identicaly gif

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,12 +4,18 @@ import { userHelperHook } from '@/_libs/servers/helper.hook';
 import HomeComponent from '@/components/pages/home.component';
 import { Metadata } from 'next';
 
-export function generateMetadata(): Promise<Metadata> {
-  const { metadata } = userHelperHook();
-  return metadata();
+interface IProps {
+  params: {
+    id: string
+  };
 }
 
-export default async function ({ params: { id } }: { params: { id: string } }) {
+export function generateMetadata({ params: { id } }: IProps): Promise<Metadata> {
+  const { metadata } = userHelperHook();
+  return metadata(id);
+}
+
+export default async function ({ params: { id } }: IProps) {
   const { fetchGiphy } = userHelperHook();
   const giphy = await fetchGiphy(id);
   
