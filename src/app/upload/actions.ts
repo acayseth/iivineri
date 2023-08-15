edcb3b2b567fdb63ace5a4b1af32448dd1d63d11
acayseth <gorbulea.s@gmail.com>
2023-08-16 01:30:17 +0300

    add history

--- src/app/upload/actions.ts
+++
@@ -1,6 +0,0 @@
-'use server';
-
-export async function handleUpload(formData: FormData) {
-  'use server';
-  console.log(formData, 'handleUpload.FormData');
-}
--- src/app/upload/page.tsx
+++
@@ -1,7 +0,0 @@
-import 'server-only';
-
-import UploadComponent from '@/components/pages/upload/upload.component';
-
-export default function UploadPage() {
-  return <UploadComponent />;
-}
